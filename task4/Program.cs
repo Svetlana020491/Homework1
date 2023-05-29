8bc1f39a4b51f44c54795adb173da474d3f61aca
Svetlana Golubeva <svetlana020491@gmail.com>
2023-05-30 01:00:13 +0500

    Решение задачи 4 разобрали на семинаре

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,14 +1,19 @@
 ﻿//  Напишите программу, которая на вход принимает число (N),
 // а на выходе показывает все чётные числа от 1 до N. 
-
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
-
-
 int multiplicity = 2;
 
-if (multiplicity <= number )
-{ }
+while (multiplicity <= number )
 
-// увы, пока не могу( понимаю что нужен цикл, но не могу сообразить
-//как все это обозначить
+{  
+      Console.Write($"{multiplicity} ");
+      multiplicity += 2;
+}
+Console.WriteLine( );
+
+
+
+
+
+
